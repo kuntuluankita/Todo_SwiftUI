56da15da4dea5ed956cba32bfa86a8e034c095e5
Kuntulu Ankita <kuntuluankita@gmail.com>
2025-03-24 23:28:58 +0530

    Initial Commit

---
+++ Todo_SwiftUI/Todo_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Todo_SwiftUIApp.swift
+//  Todo_SwiftUI
+//
+//  Created by Kuntulu Ankita on 24/03/25.
+//
+
+import SwiftUI
+
+@main
+struct Todo_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
